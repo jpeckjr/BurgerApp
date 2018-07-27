c78b9ee0048bbfd9f0fecc8397ba447ca1d14d3d
Joe Peck <jpeckjrseau@icloud.com>
2018-07-26 20:32:19 -0500

    started initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Thunder Bay Cheddar Burger');
+INSERT INTO cats (burger_name) VALUES ('Bacon Blue Cheese Burger');
+INSERT INTO cats (burger_name) VALUES ('California Burger');
